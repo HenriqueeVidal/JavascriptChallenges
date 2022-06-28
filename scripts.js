f4fcd5be7e82acbd272fa184bad85d02a784a244
Henrique Vidal <henriqueevidal@hotmail.com>
2022-06-28 18:37:24 -0300

    Adiciona script de resolução de desafio, manipulação de dados de array

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Documento JS para teste e arquivamento de scripts para resolução de problemas.
 
-function countBy(x, n) { // Função que soma o primeiro input na quantidade de vezes do segundo input, somando a cada elemento adicionado no array
+function countBy(x, n) { //Função que soma o primeiro input na quantidade de vezes do segundo input, somando a cada elemento adicionado no array
     let z = [];
     y = x
     for (let i = 0; i < n; i++) {
