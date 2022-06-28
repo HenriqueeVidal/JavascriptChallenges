d6b895424f55699372b9baa3e11005b3b57c82fb
Henrique Vidal <henriqueevidal@hotmail.com>
2022-06-28 18:35:35 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Documento JS para teste e arquivamento de scripts para resolução de problemas.
+
+function countBy(x, n) { // Função que soma o primeiro input na quantidade de vezes do segundo input, somando a cada elemento adicionado no array
+    let z = [];
+    y = x
+    for (let i = 0; i < n; i++) {
+        z.push(y)
+        y = z[z.length - 1] + x
+    }
+    return z;
+}
+
+
