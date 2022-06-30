6f4acf854ba046b9bad8b28d1333e13f34577528
Henrique Vidal <henriqueevidal@hotmail.com>
2022-06-30 19:49:13 -0300

    Adiciona função de resolução de desafio, adiciona segundo arg no array do primeiro arg e verifica se é maior que a média

--- scripts.js
+++ scripts.js
@@ -1,35 +1,43 @@
 // Documento JS para teste e arquivamento de scripts para resolução de problemas.
 
 function countBy(x, n) { //Função que soma o primeiro input na quantidade de vezes do segundo input, somando a cada elemento adicionado no array
-    let z = [];
-    y = x
-    for (let i = 0; i < n; i++) {
-        z.push(y)
-        y = z[z.length - 1] + x
-    }
-    return z;
+  let z = [];
+  y = x
+  for (let i = 0; i < n; i++) {
+    z.push(y)
+    y = z[z.length - 1] + x
+  }
+  return z;
 }
 
 
 function litres(time) { //Função que arredonda pra menos o produto da quantidade de tempo informada vezes 0,5
 
-  let lit = parseInt(parseInt(time,10)* 0.5,10)
+  let lit = parseInt(parseInt(time, 10) * 0.5, 10)
 
   return lit;
 }
 
 
-var countSheep = function (num){ //Função que concatena strings mediante quantidade desejada
-    let a = ""
-    for(let i = 1; i <= num; i++){
+var countSheep = function (num) { //Função que concatena strings mediante quantidade desejada
+  let a = ""
+  for (let i = 1; i <= num; i++) {
     a = a + i + " sheep..."
-    }
-    return a
   }
+  return a
+}
 
-  function paperwork(n, m) { //Função que verifica se argumentos são maiores que 0 e multiplica os mesmos
-    if(n<0 || m<0){
-      return 0
-    } return n*m
+function paperwork(n, m) { //Função que verifica se argumentos são maiores que 0 e multiplica os mesmos
+  if (n < 0 || m < 0) {
+    return 0
+  } return n * m
+}
+
+function betterThanAverage(classPoints, yourPoints) { // Função que adiciona a nota à array e verifica se é maior que a média retornando True or False
+  let avgr = 0
+  classPoints.push(yourPoints);
+  for (let i = 0; i < classPoints.length; i++) {
+    avgr += classPoints[i]
   }
-  
+  return yourPoints > (avgr / classPoints.length)
+}
